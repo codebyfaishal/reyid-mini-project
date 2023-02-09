7ebabc18f65249f44fc48fe9ab72258124982f20
Faishal Alif Fadhillah <personalemailisal@gmail.com>
2023-02-09 10:54:40 +0700

    add logic buat array baru

--- Components/Pokemons.tsx
+++ Components/Pokemons.tsx
@@ -16,14 +16,17 @@ import {ScrollView, FlatList} from 'react-native-gesture-handler';
 import {Button, Actionsheet, useDisclose} from 'native-base';
 import {connect} from 'react-redux';
 import {getPokemonsRequest} from '../actions/pokemons';
+import Pagination from '@cherry-soft/react-native-basic-pagination';
 
 const Pokemons = props => {
   const {getPokemonsRequest, pokemons, loading, navigation} = props;
   const {isOpen, onOpen, onClose} = useDisclose();
-  console.log('props', props)
+  console.log("PROPS", props)
   const [selectedId, setSelectedId] = useState();
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
+  const [page, setPage] = useState(1);
+  console.log(page)
 
 
   //error if no data
@@ -72,10 +75,44 @@ const onRefresh = useCallback(() => {
     );
   }
 
+
+  // logic pecah jadi 10
+  const pokemon = [...pokemons.items];
+  for (let i = 0; i < 1000; i++) {
+    pokemon.push([i]);
+  }
+  
+  let subArrays = [];
+  let subArrayCount = 0;
+  for (let i = 0; i < pokemon.length; i++) {
+    if (i % 100 === 0) {
+      subArrays[subArrayCount] = [];
+      subArrayCount++;
+    }
+    subArrays[subArrayCount - 1].push(pokemon[i]);
+  }
+  
+  const newArray = subArrays;
+  console.log('newArray', newArray)
+
+
+if (page === 2) {
+  console.log("HALAMAN 2")
+} else {
+  console.log("BUKAN")
+}
+
+
+  
+
   //Handle Button
   const handleDecline = () =>
     setIsModalVisible(() => props.navigation.navigate('Pokemons Detail'));
   const handleModal = () => setIsModalVisible(() => !isModalVisible);
+
+  
+
+
 
   //buat komponen item
   const Item = ({name, onPress}) => (
@@ -97,8 +134,22 @@ const onRefresh = useCallback(() => {
     return <Item name={item.name} onPress={() => setSelectedId(item.name)} />;
   };
 
+  const ListFooter = () => {
+    //View to set in Footer
+    return (
+      <Pagination
+      totalItems={props.pokemons.items.length}
+      pageSize={100}
+      currentPage={page}
+      onPageChange={setPage}
+      pagesToDisplay={4}
+      showLastPagesButtons
+    />
+    );
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{flex: 1}}>
       <FlatList
         data={pokemons.items}
         renderItem={renderItem}
@@ -106,6 +157,7 @@ const onRefresh = useCallback(() => {
         extraData={selectedId}
         refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
       />
+      <ListFooter></ListFooter>
       <Actionsheet isOpen={isModalVisible} disableOverlay={true}>
         <Actionsheet.Content>
           <Text style={styles.nameTitleModal}>{'bulbasaur'}</Text>
@@ -200,5 +252,16 @@ const styles = StyleSheet.create({
   },
   item: {
     width: '50%', // is 50% of container width
+  },
+  headerFooterStyle: {
+    width: '100%',
+    height: 45,
+    backgroundColor: '#606070',
+  },
+  textStyle: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    padding: 7,
   },
 });
